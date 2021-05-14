b28a3fe3072af410bd863655e49b740aacca3c8a
LucasMiguel03 <79995363+LucasMiguel03@users.noreply.github.com>
2021-05-14 02:38:26 -0300

    Adicionando a tela de Preload.

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { StackActions, NavigationActions } from 'react-navigation';
 import { connect } from 'react-redux';
 import { 
-    Container,
-    ActionButton, 
-    ActionButtonText 
-    } from './styled';
+  ScreenInitialLogin,
+  ImagePontoGraal,
+  ViewInitialLogin,
+  ViewStylePage,
+  ViewStyleLogo,
+  LogoBusaqui,
+  ViewStyleTitle,
+  Title, 
+  ViewStyleSubtitle,
+  Subtitle,
+  ButtomSignIn,
+  ButtomSignInText,
+  ButtomSubscribe,
+  ButtomSubscribeText
+} from './styled';
 
 const Preload = (props) => {
     
@@ -32,15 +43,51 @@ const Preload = (props) => {
     }
     
     return (
-        <Container>
-            <ActionButton onPress={handleSignUp}>
-                <ActionButtonText>IR go</ActionButtonText>
-            </ActionButton>
-        </Container>
+
+    <ScreenInitialLogin> 
+                {/* Imagem da primeira tela de login */}
+      <ImagePontoGraal source={require('../../Assets/Image/ImageGraal.png')} resizeMode="cover"/>    
+        {/* Container da página */}   
+        <ViewInitialLogin>
+            {/* Container do conteúdo */}
+            <ViewStylePage> 
+            {/* Container do logo Busaqui */}
+                <ViewStyleLogo>
+                {/* Logo transparente do Busaqui inserido na parte superior centralizada em linha */}
+                    <LogoBusaqui source={require('../../Assets/Image/busaqui.png')}/>
+                </ViewStyleLogo>
+                {/* container do titulo */}
+                <ViewStyleTitle>
+                {/* Título da screen */}
+                    <Title>Saiba onde</Title>
+                    <Title>seu ônibus</Title> 
+                    <Title>está agora!</Title>
+                </ViewStyleTitle>
+                    {/* Container do subtítulo */}
+                <ViewStyleSubtitle>
+                    {/* Subtítulo centralizado em linha abaixo do título */}
+                    <Subtitle>Assistente de mobilidade urbana, que </Subtitle>
+                    <Subtitle>mostra a localização em tempo real dos </Subtitle>
+                    <Subtitle>ônibus de sua cidade.</Subtitle>
+                </ViewStyleSubtitle>
+                    <ButtomSubscribe
+                        underlayColor={'#fff4'}
+                        onPress={()=> ('')}>
+                        {/* Texto do botão de entrar */}
+                        <ButtomSubscribeText>Criar sua conta</ButtomSubscribeText>
+                    </ButtomSubscribe>
+                        {/* Container do botão de cadastrar */}
+                        <ButtomSignIn
+                        onPress={()=> navigation.navigate('')} activeOpacity={0.4}>
+                        {/* Texto do botão de cadastrar */}
+                            <ButtomSignInText>Já tem conta?Entrar agora</ButtomSignInText>
+                        </ButtomSignIn>
+                    
+            </ViewStylePage>
+        </ViewInitialLogin>
+    </ScreenInitialLogin>   
     );
 }
-
-
 
  const mapStateToProps = (state) => {
      return{
--- src/screens/Preload/styled.js
+++ src/screens/Preload/styled.js
@@ -1,25 +1,107 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.KeyboardAvoidingView`
-    flex: 1;
-    background-image: url('');
-`;
+ /* --------- Estilização e criação de components (tela do Login Inical)--------*/
 
-export const ActionButton = styled.TouchableHighlight`
-    width: 153px;
-    height: 56px;
-    margin-left: 120px;
-    margin-top: 10px;
-    margin-right: 100px
-    background-color: #074DB6;
-    justify-content: center;
-    align-items: center;
-    border-radius: 5px;
-    box-shadow: 0px 2px 2px #999
-    
-`;
+     /* Estilização do Container */
+     export const ScreenInitialLogin = styled.SafeAreaView`
+     flex:1;
+     justify-content:center;
+     align-items:center;
+     display:flex;
+  `;
+       /* Estilização da imagem da primeira tela de login */
+  export const ImagePontoGraal = styled.Image`
+     flex:1;
+     display:flex;
+     width:100%;
+     background-color:#dad9d9;
+     opacity:0.737;
+  `;
+       /* Estilização do container da página */
+  export const ViewInitialLogin = styled.View`
+     width:100%;
+     height:100%;
+     margin-top:-200%;
+  `;
 
-export const ActionButtonText = styled.Text`
-    color: #FFF;
-    font-size: 16px;
-`;
+       /* Estilização do container do conteúdo */
+  export const ViewStylePage = styled.View`
+     flex-direction:column;
+     justify-content:center;
+     align-items:center;
+     display:flex;
+  `;
+      /* Estilização do container do logo */
+  export const ViewStyleLogo= styled.View`
+     margin-top:47px;
+  `; 
+      /* Estilização do Logo Busaqui */
+  export const LogoBusaqui = styled.Image`
+     width:157px;
+     height:51.02px;
+  `;
+        /* Estilização do container do título */
+  export const ViewStyleTitle = styled.View`
+     margin-top:76.98px;
+     width:270px;
+     height:135px;
+     justify-content:center;
+     align-items:center;
+  `;
+        /* Estilização do título */
+  export const Title = styled.Text`
+     font-family:Montserrat-Medium;
+     font-size:42px;
+     font-weight:normal;
+     color:#ffffff;
+     text-shadow: 1px 1px 1px #777;
+  `;
+        /* Estilização do container do subtítulo */
+  export const ViewStyleSubtitle = styled.View`
+     width:309px; 
+     height:63px;
+     justify-content:center;
+     align-items:center;
+     margin-top:21px;
+  `;
+        /* Estilização do subtítulo */
+  export const Subtitle = styled.Text`
+     font-family:Montserrat-Medium;
+     font-weight:normal;
+     font-size:13px;
+     color:#ffffff; 
+     text-shadow: 1px 1px 1px #777;
+  `;
+       /*Estilização do botão de Entrar em Login */
+  export const ButtomSubscribe = styled.TouchableHighlight`
+     width: 169px;
+     height: 56px;
+     border: 2px solid #FFFFFF;
+     border-radius: 8px;
+     justify-content:center; 
+     align-items: center; 
+     padding: 14px 18px;
+     margin-top:77px;
+   `;
+       /*Estilização do texto do botão de Entrar em Login */
+  export const ButtomSubscribeText = styled.Text`
+     color: #FFF;
+     font-size: 15px;
+     font-family:Montserrat-Medium;
+  `;
+       /*Estilização do botão de cadastrar Login */
+  export const ButtomSignIn = styled.TouchableOpacity`
+     width: 191px;
+     height: 17px;
+     margin-top:40px;
+     text-align:center;
+     justify-content:center;
+     background-color:#0000;
+  `;
+       /*Estilização do texto do botão de cadastrar Login */
+  export const ButtomSignInText = styled.Text`
+     font-size:14px;
+     color:#ffffff;
+     font-family:Montserrat-normal;
+     font-weight:normal;  
+  `;
