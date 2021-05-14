f4d4b32e7eac2903af200a3eee437ea6250ee5ff
LucasMiguel03 <79995363+LucasMiguel03@users.noreply.github.com>
2021-05-14 20:17:21 -0300

    Comentando toda a tela de GPS para ajusta-la.

--- src/screens/GPS/index.js
+++ src/screens/GPS/index.js
@@ -1,40 +1,40 @@
-import React from 'react';
+// import React from 'react';
 
-import {View} from 'react-native';
+// import {View} from 'react-native';
 
-  import {Container,GPSImage,ViewTexto,ViewImage,
-          TextoGrandeGPS,ViewButtom,TextoPequenoGPS,
-          ButtomGPS,TextoButtom} from './styled';
-  /* TODOS OS ESTILOS ESTÃO SENDO PUXADOS DE STYLE.JS */
+//   import {Container,GPSImage,ViewTexto,ViewImage,
+//           TextoGrandeGPS,ViewButtom,TextoPequenoGPS,
+//           ButtomGPS,TextoButtom} from './styled';
+//   /* TODOS OS ESTILOS ESTÃO SENDO PUXADOS DE STYLE.JS */
 
-  export default function GPSScreen(){
-    return(
-      <Container>
-          <ViewImage>
-            <GPSImage
-            source={require('../../Assets/Images/GPS.png')}/>
-          </ViewImage>
+//   export default function GPSScreen(){
+//     return(
+//       <Container>
+//           <ViewImage>
+//             <GPSImage
+//             source={require('../../Assets/Images/GPS.png')}/>
+//           </ViewImage>
 
-        <ViewTexto>
-          <TextoGrandeGPS>
-            Precisamos ter acesso a sua localização
-          </TextoGrandeGPS>
+//         <ViewTexto>
+//           <TextoGrandeGPS>
+//             Precisamos ter acesso a sua localização
+//           </TextoGrandeGPS>
 
-          <TextoPequenoGPS>
-            Para que possamos te ajudar com informações em tempo real, precisamos que você ative seu GPS.
-          </TextoPequenoGPS>
-        </ViewTexto>
-        <ViewButtom>
-          <ButtomGPS
-          onpress={()=>{}}>
-            <TextoButtom>
-                Ativar Agora
-            </TextoButtom>
-          </ButtomGPS>
-        </ViewButtom>
+//           <TextoPequenoGPS>
+//             Para que possamos te ajudar com informações em tempo real, precisamos que você ative seu GPS.
+//           </TextoPequenoGPS>
+//         </ViewTexto>
+//         <ViewButtom>
+//           <ButtomGPS
+//           onpress={()=>{}}>
+//             <TextoButtom>
+//                 Ativar Agora
+//             </TextoButtom>
+//           </ButtomGPS>
+//         </ViewButtom>
         
-      </Container>
-    );
-  }
+//       </Container>
+//     );
+//   }
 
     
--- src/screens/GPS/styled.js
+++ src/screens/GPS/styled.js
@@ -1,81 +1,81 @@
-import styled from 'styled-components/native';
-import color from '../../styles/color';
-import fonts from '../../styles/fonts';
+// import styled from 'styled-components/native';
+// import color from '../../styles/color';
+// import fonts from '../../styles/fonts';
 
-////////////////
-//TELA DE GPS//
-//////////////
+// ////////////////
+// //TELA DE GPS//
+// //////////////
 
-//Container da pagina Inteira - SafeAreaView
-export const Container = styled.SafeAreaView`
-    background-color: ${color.Branco};
-    flex:1;
-    justify-content:space-evenly;
-`;
+// //Container da pagina Inteira - SafeAreaView
+// export const Container = styled.SafeAreaView`
+//     background-color: ${color.Branco};
+//     flex:1;
+//     justify-content:space-evenly;
+// `;
 
-//Imagem da tela de GPS
-export const GPSImage = styled.Image`
-    /*width:248.11px;
-    height:152px;*/
-    width:320px;
-    height:205px;
-    align-self:center;
-`;
+// //Imagem da tela de GPS
+// export const GPSImage = styled.Image`
+//     /*width:248.11px;
+//     height:152px;*/
+//     width:320px;
+//     height:205px;
+//     align-self:center;
+// `;
 
-export const ViewImage = styled.View`
-    flex:4;
-    justify-content:center;
+// export const ViewImage = styled.View`
+//     flex:4;
+//     justify-content:center;
     
-`;
+// `;
 
-export const ViewTexto = styled.View`
-    flex:3;
-    justify-content:space-evenly;
+// export const ViewTexto = styled.View`
+//     flex:3;
+//     justify-content:space-evenly;
     
-`;
+// `;
 
-export const ViewButtom = styled.View`
-    flex:2;
+// export const ViewButtom = styled.View`
+//     flex:2;
     
-    justify-content:center;
-`
+//     justify-content:center;
+// `
 
-//Texto grande da tela de GPS
-export const TextoGrandeGPS = styled.Text`
-    font-size: 29px;
-    line-height: 32px;
-    text-align: center;
-    font-family: ${fonts.SemiBold};
-    color: ${color.CinzaEscuro};
-    width: 321.62px;
-    align-self:center;
-`;
+// //Texto grande da tela de GPS
+// export const TextoGrandeGPS = styled.Text`
+//     font-size: 29px;
+//     line-height: 32px;
+//     text-align: center;
+//     font-family: ${fonts.SemiBold};
+//     color: ${color.CinzaEscuro};
+//     width: 321.62px;
+//     align-self:center;
+// `;
 
-//Texto pequeno da tela de GPS
-export const TextoPequenoGPS = styled.Text`
-    font-size: 13px;
-    text-align: center;
-    line-height: 20px;
-    width: 305px;
-    font-family: ${fonts.Medium};
-    align-self:center;
-    color: ${color.Cinza}
-`;
+// //Texto pequeno da tela de GPS
+// export const TextoPequenoGPS = styled.Text`
+//     font-size: 13px;
+//     text-align: center;
+//     line-height: 20px;
+//     width: 305px;
+//     font-family: ${fonts.Medium};
+//     align-self:center;
+//     color: ${color.Cinza}
+// `;
 
-//Botão da tela de GPS
-export const ButtomGPS = styled.TouchableOpacity`
-    width: 150px;
-    height: 56px;
-    border-radius: 8px;
-    background-color: ${color.Azul};
-    align-items: center;
-    justify-content: center;
-    align-self:center;
-`;
+// //Botão da tela de GPS
+// export const ButtomGPS = styled.TouchableOpacity`
+//     width: 150px;
+//     height: 56px;
+//     border-radius: 8px;
+//     background-color: ${color.Azul};
+//     align-items: center;
+//     justify-content: center;
+//     align-self:center;
+// `;
 
-//Texto do botão de entrar
-export const TextoButtom = styled.Text`
-    color: ${color.Branco};
-    font-size: 18px;
-    font-family: ${fonts.SemiBold};
-`;
+// //Texto do botão de entrar
+// export const TextoButtom = styled.Text`
+//     color: ${color.Branco};
+//     font-size: 18px;
+//     font-family: ${fonts.SemiBold};
+// `;
